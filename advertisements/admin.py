66c8c469e03d49ba57b87d70eeb13e9d4b4ccfab
Gilang R Ilhami <119667058+gilangrilhami@users.noreply.github.com>
2022-12-23 19:40:17 +0000

    [FEAT] Advertisements App and Models Registraiton
    Register advertisements app in
    `settings.py` and models in the
    django adming page where the
    `AdvertisementGroupTargetingRuleInline`
    and `Advertisement` can be
    configured inline with `AdvertisementGroup`

--- advertisements/admin.py
+++ advertisements/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from .models import (
+    Advertiser,
+    Campaign,
+    AdvertisementGroup,
+    Advertisement,
+    AdvertisementGroupTargetingRule
+)
+
 # Register your models here.
+
+class AdvertisementInline(admin.StackedInline):
+    model = Advertisement
+    fk_name = 'advertisement_group'
+
+class AdvertisementGroupTargetingRuleInline(admin.StackedInline):
+    model = AdvertisementGroupTargetingRule
+    fk_name = 'advertisement_group'
+
+class AdvertisementGroupAdmin(admin.ModelAdmin):
+    inlines = [AdvertisementInline, AdvertisementGroupTargetingRuleInline]
+
+admin.site.register(Advertiser)
+admin.site.register(Campaign)
+admin.site.register(AdvertisementGroup, AdvertisementGroupAdmin)
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ if 'CODESPACE_NAME' in os.environ:
 
 INSTALLED_APPS = [
     "sites.apps.SitesConfig",
+    "advertisements.apps.AdvertisementsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
