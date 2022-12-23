7e272c120602a96033d764961b177614637c3d13
Gilang R Ilhami <119667058+gilangrilhami@users.noreply.github.com>
2022-12-23 18:58:23 +0000

    [FEAT] Models in Sites App
    Add necessary models based
    on requirements. Models
    created are the following:
    
    - `Publisher`
    - `Site` (FK to `Publisher`)
    - `Slot` (FK to `Site`)

--- sites/models.py
+++ sites/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Publisher(models.Model):
+    guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+
+class Site(models.Model):
+    guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, related_name='sites')
+    name = models.CharField(max_length=255)
+
+class Slot(models.Model):
+    guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='slots')
+    name = models.CharField(max_length=255)
