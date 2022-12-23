33abc597652e7f513b6d04e42e894f222f225532
Gilang R Ilhami <119667058+gilangrilhami@users.noreply.github.com>
2022-12-23 19:24:32 +0000

    [FEAT] Site and Advertisements models
    Create models for Advertisements
    based on requirements. models
    include the following:
    
    - `Advertiser`
    - `Campaign` (FK to `Advertiser`)
    - `AdvertisementGroup`
      (FK to `Campaign` )
    - `Advertisement`
      (FK to `AdvertisementGroup`)
    - `AdvertisementGroupTargetingRule`
      (FK to `AdvertisementGroup` &
      M:M to `Slot`)
    
    Also modifed the `guid` fields for
    all Site models to use `unique=True`
    instead of `primary_key=True`

--- advertisements/models.py
+++ advertisements/models.py
@@ -1,3 +1,30 @@
+import uuid
 from django.db import models
+from sites.models import Slot
 
 # Create your models here.
+
+class Advertiser(models.Model):
+    guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+
+class Campaign(models.Model):
+    guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE, related_name='campaigns')
+    name = models.CharField(max_length=255)
+
+class AdvertisementGroup(models.Model):
+    guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE, related_name='advertisement_groups')
+    name = models.CharField(max_length=255)
+
+class AdvertisementGroupTargetingRule(models.Model):
+    advertisement_group = models.ForeignKey(AdvertisementGroup, on_delete=models.CASCADE, related_name='targeting_rules')
+    slot = models.ManyToManyField(Slot, related_name='targeting_rules')
+    tags = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+class Advertisement(models.Model):
+    guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    advertisement_group = models.ForeignKey(AdvertisementGroup, on_delete=models.CASCADE, related_name='advertisements')
+    data = models.TextField()
--- sites/models.py
+++ sites/models.py
@@ -4,15 +4,15 @@ from django.db import models
 # Create your models here.
 
 class Publisher(models.Model):
-    guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
 
 class Site(models.Model):
-    guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, related_name='sites')
     name = models.CharField(max_length=255)
 
 class Slot(models.Model):
-    guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='slots')
     name = models.CharField(max_length=255)
