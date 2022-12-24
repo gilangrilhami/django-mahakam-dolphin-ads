5bcadfd82b5614adc812ec33f35b582e0456b840
Gilang R Ilhami <119667058+gilangrilhami@users.noreply.github.com>
2022-12-24 19:10:59 +0000

    [FEAT] Add dashboard app models to admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import DenormalizedAdvertisement
+
 # Register your models here.
+
+class DenormalizedAdvertisementAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        # Use the 'denormalized_db' database for the queryset
+        return super().get_queryset(request).using('dashboard')
+        
+    def has_add_permission(self, request):
+        # Disable the 'Add' button in the Django admin
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        # Disable the 'Save' and 'Save and continue editing' buttons in the Django admin
+        return False
+
+admin.site.register(DenormalizedAdvertisement, DenormalizedAdvertisementAdmin)
