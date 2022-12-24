b638ecc650bcefc3c9e2ca8f366d7e2ee69d4b17
Gilang R Ilhami <119667058+gilangrilhami@users.noreply.github.com>
2022-12-24 04:09:35 +0000

    [FEAT] Dashboard App
    Create a new Django app called
    `dashboard` which will contain
    the denormalized data of
    advertisements.

--- project/settings.py
+++ project/settings.py
@@ -87,6 +87,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    },
+    "dashboard": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "dashboard-db.sqlite3",
     }
 }
 
